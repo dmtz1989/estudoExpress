e7aa4436b5866372d5468f1bb97f8825cc40b0aa
dmtz1989 <demetryus.oliveira@gmail.com>
2021-03-31 14:47:52 -0300

    atualizando index.js

--- index.js
+++ index.js
@@ -1,14 +1,56 @@
 const express = require('express');
+const {response} = require('express');
+const {v4:uuidv4} = require('uuid');
 
 const app = express();
 
-app.get('/', (request, response) => {
-    response.send('Achou que era um "Olá, Mundo!", mas sou eu o DIO!');
-})
+app.use(express.json());
+const projects = [];
+/**
+ *  Query Params: Usado principalmente para filtros e paginação
+ *  Route Params: identificar os recursos antes de deletar eles
+ *  Request Params: 
+ *  * */
+app.get('/projects', (request, response) => {
+   return response.json(projects);
+});
+
+app.post('/projects', (request, response) => {
+    const {title, owner} = request.body;
+   
+    const project = { id: uuidv4(), title, owner};
+    projects.push(project);
+    console.log(project.id);
+
+    return response.json(project);
+    //console.log(title);
+    //console.log(owner);
+
+//return response.json([
+  //  'Projeto1',
+   // 'Projeto2',
+    //'Projeto3'
+});
+
+app.put('/projects/:id', (request, response) =>{
+    const params =  request.params;
+
+    console.log(params);
+
+    return response.json([
+        'Projeto 50',
+        'Projeto 2'
+    ]);
+});
+
+app.delete('/projects/:id', (request, response) =>{
+    return response.json([
+        'Projeto 50',
+        'Projeto 2'
+    ]);
+});
 
 app.listen(3000, () => {
-    console.log('Servidor esta rodando!');
-})
-
-
+    console.log('Servidor esta rodando! \n');
+});
 //console.log(app);
